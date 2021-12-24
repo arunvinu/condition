6cbd4266edde266602d7d133c37c643ee569d931
arunvinu <arunkumarxworkz@gmail.com>
2021-12-24 14:15:50 +0545

    condition

---
+++ Weekday.java
@@ -0,0 +1,20 @@
+class Weekday{
+public static void main (String args[ ]){
+int days =1;
+if(days==1){
+	System.out.println("sunday");
+}else if(days==2){
+	System.out.println("Monday");
+}else if(days==3){
+	System.out.println("tuesday");
+}else if(days==4){
+	System.out.println("wednesday");
+}else if(days==5){
+	System.out.println("thursday");
+}else if(days==6){
+	System.out.println("friday");
+}else if(days==7){
+	System.out.println("saturday");
+}
+}
+}
