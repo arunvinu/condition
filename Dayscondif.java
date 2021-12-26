0a716e778370033bb72f6989d62f790d45b7a4bb
arunvinu <96110638+arunvinu@users.noreply.github.com>
2021-12-26 14:14:06 +0545

    Add files via upload

---
+++ Dayscondif.java
@@ -0,0 +1,8 @@
+class Dayscondif{
+public static void main(String args[ ]){
+boolean day = true;
+if(day=true){
+System.out.println("Sunday");
+}
+}
+}
---
+++ Monthyear.java
@@ -0,0 +1,30 @@
+class Monthyear{
+public static void main (String args[ ]){
+int a =12;
+if(a==1){
+	System.out.println("january");
+}else if(a==2){
+	System.out.println("february");
+}else if(a==3){
+	System.out.println("march");
+}else if(a==4){
+	System.out.println("april");
+}else if(a==5){
+	System.out.println("may");
+}else if(a==6){
+	System.out.println("june");
+}else if(a==7){
+	System.out.println("july");
+}else if(a==8){
+	System.out.println("august");
+}else if(a==9){
+	System.out.println("september");
+}else if(a==10){
+	System.out.println("october");
+}else if(a==11){
+	System.out.println("november");
+}else if(a==12){
+	System.out.println("december");
+}
+}
+}
